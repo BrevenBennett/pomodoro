7e4152d7e3bdef9c1d28db8c6ad05168389c9d84
BrevenBennett <Breven.stars@gmail.com>
2023-12-26 21:17:27 -0600

    made start and stop buttons disabled if already selected

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const timerMinutes = document.querySelector(".timer__minutes");
 const timerSeconds = document.querySelector(".timer__seconds");
 const timerMilliseconds = document.querySelector(".timer__milliseconds");
+const startButton = document.querySelector(".stopwatch__start");
+const stopButton = document.querySelector(".stopwatch__stop");
+const resetButton = document.querySelector(".stopwatch__reset");
 
 let startTime;
 let cancelId;
@@ -8,11 +11,19 @@ let savedTime = 0;
 const countdown = 25 * 60 * 1000;
 
 function start() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = false;
+
   startTime = Date.now();
   cancelId = requestAnimationFrame(updateTimer);
 }
 
 function stop() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+
   savedTime += Date.now() - startTime;
   cancelAnimationFrame(cancelId);
 }
@@ -32,12 +43,16 @@ const zeroPad = (num, places) => {
 
 function updateTimer() {
   let millisElapsed = Date.now() - startTime + savedTime;
-
   let millisLeft = countdown - millisElapsed;
+  if (millisLeft < 0) {
+    millisLeft = 0;
+    cancelAnimationFrame(cancelId);
+    cancelId = null;
+  }
   let secondsLeft = millisLeft / 1000;
   let minutesLeft = secondsLeft / 60;
 
-  timerMilliseconds.innerHTML = zeroPad((millisLeft % 1000), 3);
+  timerMilliseconds.innerHTML = zeroPad(millisLeft % 1000, 3);
   timerSeconds.innerHTML = zeroPad(Math.floor(secondsLeft % 60), 2);
   timerMinutes.innerHTML = zeroPad(Math.floor(minutesLeft), 2);
 
